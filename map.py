24c7932e9542318d69ba67ad6835f357b20cc460
mourya.polaka@gmail.com <mourya.polaka@gmail.com>
2020-09-12 17:27:38 +1000

    Build upload button to map coordinates

---
+++ map.py
@@ -0,0 +1,32 @@
+from flask import Flask, render_template, request
+import folium
+import pandas as pd
+import csv
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    return render_template('index.html')
+
+@app.route('/map_plot', methods=['GET', 'POST'])
+def map_plot():
+    #Map object
+    map = folium.Map(
+        location=[-31.840233, 145.612793],
+        zoom_start=6)
+
+    if request.method == 'POST':
+        f = request.form['csvfile']
+        with open(f) as file:
+            data = pd.read_csv(file)         
+            for index, row in data.iterrows():
+                folium.Marker(location=[row['lat'], row['long']],
+                    popup=row['suburb']).add_to(map)
+
+        map.save('templates/map.html'),
+
+        return render_template('map.html', data=data)
+
+if __name__ == '__main__':
+    app.run(debug=True)
